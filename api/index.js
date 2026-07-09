15972d8eb0796d3c736764dd18cdca37c2aca007
JCPrime01 <vanzokiller@gmail.com>
2026-07-09 11:58:20 -0300

    chore: shareCode 17ZMO6DAICM -> HLY2NU8PFPZ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=17ZMO6DAICM&afp1=bd&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=HLY2NU8PFPZ&afp1=bd&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
