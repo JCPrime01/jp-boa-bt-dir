392dc6eb5269ff2f059fb4ff9ee3ed6693796a06
JCPrime01 <vanzokiller@gmail.com>
2026-07-15 12:34:01 -0300

    chore: shareCode YQMD1VM9HV0 -> E981U8JLVX3 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=YQMD1VM9HV0&afp1=bd&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=E981U8JLVX3&afp1=bd&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
