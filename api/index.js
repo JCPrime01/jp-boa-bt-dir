95b7baf4eace5cfc3baf32293bfff55b206b609f
JCPrime01 <vanzokiller@gmail.com>
2026-07-29 09:48:23 -0300

    chore: shareCode UT0M3B7SQNX -> 87T7FNKC63E (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=UT0M3B7SQNX&afp1=bd&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=87T7FNKC63E&afp1=bd&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
