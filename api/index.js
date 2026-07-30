c20461a05cc4db50b9a72294942ab132470f6812
JCPrime01 <vanzokiller@gmail.com>
2026-07-30 11:22:34 -0300

    chore: shareCode 87T7FNKC63E -> ACZEEW0ZCMY (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=87T7FNKC63E&afp1=bd&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=ACZEEW0ZCMY&afp1=bd&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
