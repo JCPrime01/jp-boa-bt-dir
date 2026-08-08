7abb0fbf6f7feda4ad61d18cccecfe86eb9f6ef0
JCPrime01 <vanzokiller@gmail.com>
2026-08-08 11:49:09 -0300

    chore: shareCode VG41PMM6YL3 -> IC3VFX3SC8B (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=VG41PMM6YL3&afp1=bd&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=IC3VFX3SC8B&afp1=bd&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
