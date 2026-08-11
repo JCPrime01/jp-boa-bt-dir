dabd3423be57e55c9ce41a5fc671ab4ddbf7e826
JCPrime01 <vanzokiller@gmail.com>
2026-08-11 12:54:49 -0300

    chore: shareCode MD30XOT49W8 -> E6GC9OJMRID (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=MD30XOT49W8&afp1=bd&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=E6GC9OJMRID&afp1=bd&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
